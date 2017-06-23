bda20dd2a7aca3eb65600802089644f5dcf75970
Bruin324 <bruin324@gmAIL.COM>
2017-06-23 14:02:08 -0400

    register/login working

---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const mustache = require('mustache-express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+const session = require('express-session');
+
+var application = express();
+application.engine('mustache', mustache());
+
+application.set('views', './views');
+application.set('view engine', 'mustache');
+
+var users = [{
+    email: 'admin@test', 
+    name: 'Bruin', 
+    password: 'qwer4321',
+    views: 0,
+    isAuthenticated: false
+}];
+
+
+
+application.use(cookieParser());
+application.use(bodyParser.urlencoded());
+application.use(session({
+    secret: 'bruinisinthehouse',
+    resave: false,
+    saveUninitialized: true
+}))
+
+application.get('/', (request, response) => {
+    response.render('index');
+})
+
+application.get('/login', (request, response) => {
+    response.render('login');
+})
+
+application.get('/register', (request, response) => {
+    response.render('register');
+});
+
+application.post('/register', (request, response) => {
+    var user = {
+        name: request.body.name, 
+        email: request.body.email,
+        password: request.body.password, 
+        views: 0, 
+        isAuthenticated: true
+    }
+    users.push(user);
+    request.session.name = request.body.name;
+    request.session.email = request.body.email;
+    request.session.views = 1;
+    request.session.isAuthenticated = true;
+
+    response.redirect('dashboard')
+    
+})
+
+application.post('/login', (request, response) => {
+    var user = users.find(user => {return user.email === request.body.email && user.password === request.body.password});
+    if (user) {
+        request.session.isAuthenticated = true;
+        request.session.name = user.name;
+        request.session.email = user.email;
+        request.session.views = 1;
+
+        response.redirect('dashboard');
+    } else {
+        response.render('login', user)
+    }
+});
+
+application.get('/dashboard', (request, response) => {
+
+    if(request.session.isAuthenticated === false) {
+        render.redirect('login');
+    }
+    else {
+        var user = {
+            name: request.session.name,
+            email: request.session.email,
+            views: request.session.views++
+        }
+    }
+    
+    response.render('dashboard', user)
+})
+
+application.listen(3000);
+console.log('application started');
